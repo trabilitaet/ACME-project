35606655deac12765e0baa88f9f395cfb25092f5
Silvia <silvia.ritsch@scs.ch>
2020-10-10 15:53:18 +0200

    dns: listen and receive dns requests

--- src/serve-dns.go
+++ src/serve-dns.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"net"
 )
 
+const DNSPort = 11053
 // TODO:
 // * basic: serve DNS requests
 // * reply to challenges `dns-01`
@@ -14,4 +16,20 @@ import (
 // suggest localhost for all requests? port?
 
 func main() {
+	//Listen on UDP Port 11053 of localhost
+	addr := net.UDPAddr{
+		Port: DNSPort,
+		IP:   net.ParseIP("127.0.0.1"),
+	}
+	udp, _ := net.ListenUDP("udp", &addr)
+
+	// infinite loop to wait for requests
+	for {
+	    packet := make([]byte, 1024) // binary
+		len, addr, _ := udp.ReadFrom(packet)
+		fmt.Print("packet received\n")
+		fmt.Println(packet)
+		fmt.Println(len)
+		fmt.Println(addr)
+	}
 }
